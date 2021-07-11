7cb1246b45ea9b274cc41539e72cadee7626d54d
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2021-07-11 13:36:39 +0200

    refactor: heavily simplifying the ServiceWorker script again

--- loading-attribute-polyfill.sw.js
+++ loading-attribute-polyfill.sw.js
@@ -1,17 +1,20 @@
 self.addEventListener('fetch', (event) => {
-	const requestURLParams = new URL(event.request.url).searchParams;
+	const requestURLParams = new URL(event.request.url).searchParams,
+		swURLSearchparams = new URL(self.location.href).searchParams;
 
 	if (
 		requestURLParams.get('loading') &&
 		requestURLParams.get('loading') === 'lazy'
 	) {
 		if (event.request.destination === 'iframe') {
-			event.respondWith(
-				new Response('', {
-					headers: { 'Content-Type': 'text/html; charset=utf-8' },
-				})
-			);
-		} else {
+			if (swURLSearchparams.get('loading-iframes') !== 'true') {
+				event.respondWith(
+					new Response('', {
+						headers: { 'Content-Type': 'text/html; charset=utf-8' },
+					})
+				);
+			}
+		} else if (swURLSearchparams.get('loading-images') !== 'true') {
 			event.respondWith(
 				new Response(
 					'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ' +
