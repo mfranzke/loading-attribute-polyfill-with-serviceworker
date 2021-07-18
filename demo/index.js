5cf208dd46c5867273db4f9bd9b0127113bfa7a7
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2021-07-18 14:31:38 +0200

    feat: added possibility to add dynamic content elements
    images and iframes

--- demo/index.js
+++ demo/index.js
@@ -1,2 +1,38 @@
 import loadingAttributePolyfill from '../dist/loading-attribute-polyfill.module.js';
 
+// Test for dynamically inserted images and iframes
+let addDynamicContent = (event) => {
+	let divElement = document.createElement('div'),
+		imageElement = document.createElement('img'),
+		iframeElement = document.createElement('iframe');
+
+	imageElement.setAttribute(
+		'src',
+		'img/lazyimg-src-loadinglazy.250x150.guetzli.jpg?loading=lazy&image-width=250&image-height=150'
+	);
+	imageElement.setAttribute('loading', 'lazy');
+	imageElement.setAttribute('alt', '..');
+	imageElement.setAttribute('width', '250');
+	imageElement.setAttribute('height', '150');
+
+	iframeElement.setAttribute('src', 'iframe.html?loading=lazy');
+	iframeElement.setAttribute('loading', 'lazy');
+	iframeElement.setAttribute('title', '..');
+	iframeElement.setAttribute('width', '320');
+	iframeElement.setAttribute('height', '180');
+
+	divElement.appendChild(imageElement);
+	divElement.appendChild(iframeElement);
+
+	document.querySelector('main').insertAdjacentElement('beforeend', divElement);
+
+	// Call for preparing the sample image element included the latest
+	loadingAttributePolyfill.prepareElement(divElement.querySelector('img'));
+	loadingAttributePolyfill.prepareElement(divElement.querySelector('iframe'));
+
+	event.preventDefault();
+};
+
+document
+	.querySelector('button.add-dynamic-content')
+	.addEventListener('click', addDynamicContent);
